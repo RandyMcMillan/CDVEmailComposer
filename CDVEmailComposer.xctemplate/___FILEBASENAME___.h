9110d034b54b9f0fea5fce5fb78544bf5777b756
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-01-07 15:09:19 -0500

    new file:   CDVEmailComposer.xctemplate/TemplateIcon.icns 	new file:   CDVEmailComposer.xctemplate/TemplateInfo.plist 	new file:   CDVEmailComposer.xctemplate/___FILEBASENAME___.h 	new file:   CDVEmailComposer.xctemplate/___FILEBASENAME___.js 	new file:   CDVEmailComposer.xctemplate/___FILEBASENAME___.m 	new file:   CDVEmailComposer.xctemplate/___FILEBASENAME___.md 	new file:   CDVEmailComposer.xctemplate/index.html 	new file:   Icon 	new file:   "Icon\r" 	new file:   README.md

---
+++ CDVEmailComposer.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,18 @@
+//
+//  EmailComposer.h
+//
+//
+//  Created by Jesse MacFadyen on 10-04-05.
+//  Copyright 2010 Nitobi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MessageUI/MFMailComposeViewController.h>
+#import <Cordova/CDVPlugin.h>
+
+
+@interface ___FILEBASENAME___ : CDVPlugin oseViewControllerDelegate >{}
+
+- (void) show___FILEBASENAME___:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+
+@end
---
+++ CDVEmailComposer.xctemplate/___FILEBASENAME___.js
@@ -0,0 +1,59 @@
+function ___FILEBASENAME___() {
+	this.resultCallback = null; // Function
+}
+
+___FILEBASENAME___.ComposeResultType = {
+Cancelled:0,
+Saved:1,
+Sent:2,
+Failed:3,
+NotSent:4
+}
+
+
+
+// showEmailComposer : all args optional
+
+___FILEBASENAME___.prototype.showEmailComposer = function(subject,body,toRecipients,ccRecipients,bccRecipients,bIsHTML) {
+	var args = {};
+	if(toRecipients)
+		args.toRecipients = toRecipients;
+	if(ccRecipients)
+		args.ccRecipients = ccRecipients;
+	if(bccRecipients)
+		args.bccRecipients = bccRecipients;
+	if(subject)
+		args.subject = subject;
+	if(body)
+		args.body = body;
+	if(bIsHTML)
+		args.bIsHTML = bIsHTML;
+	
+	cordova.exec(null, null, "EmailComposer", "showEmailComposer", [args]);
+}
+
+// this will be forever known as the orch-func -jm
+___FILEBASENAME___.prototype.showEmailComposerWithCB = function(cbFunction,subject,body,toRecipients,ccRecipients,bccRecipients,bIsHTML) {
+	this.resultCallback = cbFunction;
+	this.showEmailComposer.apply(this,[subject,body,toRecipients,ccRecipients,bccRecipients,bIsHTML]);
+}
+
+___FILEBASENAME___.prototype._didFinishWithResult = function(res) {
+	this.resultCallback(res);
+}
+
+
+
+cordova.addConstructor(function()  {
+					   if(!window.plugins)
+					   {
+					   window.plugins = {};
+					   }
+					   
+					   // shim to work in 1.5 and 1.6
+					   if (!window.Cordova) {
+					   window.Cordova = cordova;
+					   };
+					   
+					   window.plugins.___FILEBASENAME___ = new ___FILEBASENAME___();
+					   });
